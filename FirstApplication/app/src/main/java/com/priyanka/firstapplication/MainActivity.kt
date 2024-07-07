a679cd53af2bc1fa6f53f7407b0c7498d4686720
Pinku2004 <cse.22bcsi54@silicon.ac.in>
2024-07-07 19:20:45 +0530

    Added

---
+++ FirstApplication/app/src/main/java/com/priyanka/firstapplication/MainActivity.kt
@@ -0,0 +1,253 @@
+package com.priyanka.firstapplication
+/*
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.priyanka.firstapplication.ui.theme.FirstApplicationTheme
+*/
+
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.priyanka.firstapplication.ui.theme.FirstApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                       /* HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        NormalText()
+                       /* HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        BoldPurpleText()
+                        /*HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        GradientText()
+                        /*HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        ShadowText()
+                        /*HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        StyledText()
+                        /*HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                        OpacityText()
+                        /*HorizontalDivider(
+                            modifier = Modifier.padding(vertical = 3.dp),
+                            thickness = 2.dp,
+                            color = Color.DarkGray
+                        )*/
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Application Lifecycle is in Start state")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Application Lifecycle is in Resume state")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Application Lifecycle is in Pause state")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Application Lifecycle is in Stop state")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Application Lifecycle is in Destroy state")
+    }
+}
+
+@Composable
+fun NormalText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "I am PRIYANKA ", fontSize = 36.sp)
+    }
+}
+
+@Composable
+fun BoldPurpleText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "I am PRIYANKA",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFBB86FC)
+        )
+    }
+}
+
+@Composable
+fun GradientText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "I am PRIYANKA",
+            fontSize = 36.sp,
+            style = TextStyle(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF98FF98),  // Mint green
+                        Color(0xFF00FF00),  // Green
+                        Color(0xFA00EF00),  // Greenish shade
+                        Color(0xFF00FFFF),  // Cyan
+                        Color(0xFF7FFFD4),  // Aquamarine
+                        Color(0xFF40E0D0),  // Turquoise
+                        Color(0xFF48D1CC),  // Medium turquoise
+                        Color(0xFF00CED1),  // Dark turquoise
+                        Color(0xFF20B2AA),  // Light sea green
+                        Color(0xFF5F9EA0)   // Cadet blue
+                    )
+                )
+            )
+        )
+    }
+}
+
+@Composable
+fun ShadowText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        val offset = Offset(5.0f, 10.0f)
+        Text(
+            text = "I am PRIYANKA",
+            style = TextStyle(
+                fontSize = 36.sp,
+                shadow = Shadow(
+                    color = Color.Blue, offset = offset, blurRadius = 3f
+                )
+            )
+        )
+    }
+}
+
+@Composable
+fun StyledText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Cyan)) {
+                    append("I")
+                }
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Magenta)) {
+                    append(" a")
+                }
+                append("m")
+                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Blue)) {
+                    append(" P")
+                }
+                append("RIYANKA")
+            },
+            fontSize = 36.sp
+        )
+    }
+}
+
+@Composable
+fun OpacityText() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color(0x80BB86FC))) {
+                    append("I")
+                }
+                withStyle(style = SpanStyle(color = Color(0x800000FF))) {
+                    append(" am")
+                }
+                withStyle(style = SpanStyle(color = Color(0xFF00CED1))) {
+                    append(" PRIYANKA")
+                }
+            },
+            fontSize = 36.sp
+        )
+    }
+}
